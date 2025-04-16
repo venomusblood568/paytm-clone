f18b6e2c2cff021b241ce063b9cd35ce3c18788b
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-04-17 00:50:28 +0530

    backend setup

--- be/dist/index.js
+++ be/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const PORT = process.env.PORT || 3000;
+const app = (0, express_1.default)();
+app.get('/', (Reqest, Response) => {
+    Response.send("pay backend working fine");
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- be/src/index.ts
+++ be/src/index.ts
@@ -1,2 +1,13 @@
-const express = require("express")
+
+import express from "express";
+
+const PORT = process.env.PORT || 3000
 const app = express()
+
+app.get('/', (Reqest,Response) => {
+    Response.send("pay backend working fine")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
